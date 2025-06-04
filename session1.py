180c833a6b146262ac62c0f89b5284036fafd88f
Enoch Owoade <enochowoade@gmail.com>
2025-06-04 03:00:13 +0000

    problem 3

--- session1.py
+++ session1.py
@@ -9,3 +9,33 @@ def greeting(name):
     print("Welcome to The Hundred Acre Wood "+ name+"! My name is Christopher Robin.")
 
 greeting("Samuel")
+
+# Problem 3
+def print_catchphrase(character):
+
+    catchphrases = {
+
+        "Pooh": "Oh bother!",
+
+        "Tigger": "TTFN: Ta-ta for now!",
+
+        "Eeyore": "Thanks for noticing me.",
+
+        "Christopher Robin": "Silly old bear."
+
+    }
+
+    if character in catchphrases:
+
+        print(catchphrases[character])
+
+    else:
+
+        print(f"Sorry! I don't know {character}'s catchphrase!")
+
+
+character = "Pooh"
+print_catchphrase(character)
+
+character = "Piglet"
+print_catchphrase(character)
