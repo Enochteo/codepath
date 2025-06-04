e9a14d3425d42673742e7b4d191a457aad83c42d
Enoch Owoade <enochowoade@gmail.com>
2025-06-04 02:59:27 +0000

    problem 1 and 2

---
+++ session1.py
@@ -0,0 +1,11 @@
+# Problem 1
+def welcome():
+    print("Welcome to The Hundred Acre Wood!")
+
+welcome()
+
+# Problem 2
+def greeting(name):
+    print("Welcome to The Hundred Acre Wood "+ name+"! My name is Christopher Robin.")
+
+greeting("Samuel")
