f4a714ec612e82632bd9e70917ca329165c030c8
Enochteo <enochowoade@gmail.com>
2025-07-17 12:46:17 -0500

    add new problem s1

---
+++ Wk7/session1.py
@@ -0,0 +1,11 @@
+def count_layers(sandwich):
+    #print(sandwich)
+    if len(sandwich) == 1:
+        return 1
+    return 1 + count_layers(sandwich[1])
+
+sandwich1 = ["bread", ["lettuce", ["tomato", ["bread"]]]]
+sandwich2 = ["bread", ["cheese", ["ham", ["mustard", ["bread"]]]]]
+
+print(count_layers(sandwich1))
+print(count_layers(sandwich2))
