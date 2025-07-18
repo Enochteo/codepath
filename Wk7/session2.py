859db93cee3c9a369d2a9732a8f2ca279bb0444b
Enochteo <enochowoade@gmail.com>
2025-07-18 16:53:29 -0500

    add new problem

--- Wk7/session2.py
+++ Wk7/session2.py
@@ -195,3 +195,12 @@ print(next_greatest_letter(letters, 3))
 
 # Problem 5
 print("Problem 5")
+# binary search
+def find_closest_planets(planets, target_distance, k):
+    pass
+
+planets1 = [100, 200, 300, 400, 500]
+planets2 = [10, 20, 30, 40, 50]
+
+print(find_closest_planets(planets1, 350, 3))
+print(find_closest_planets(planets2, 25, 2))
