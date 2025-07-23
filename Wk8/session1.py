fb8741edd7073d4f4410008d13f2acdcf9172cff
Enochteo <enochowoade@gmail.com>
2025-07-23 14:41:12 -0400

    add problem 8

--- Wk8/session1.py
+++ Wk8/session1.py
@@ -107,8 +107,8 @@ root.right.right = TreeNode(2)
 
 print(calculate_yield(root))
 
-# Problem 5
-print("problem 5")
+# Problem 6
+print("problem 6")
 
 class TreeNode:
     def __init__(self, value, left=None, right=None):
@@ -134,8 +134,8 @@ plant_taxonomy = TreeNode("Plantae",
 
 print(get_most_specific(plant_taxonomy))
 
-# Problem 5
-print("problem 5")
+# Problem 7
+print("problem 7")
 
 class TreeNode:
     def __init__(self, value, left=None, right=None):
@@ -157,3 +157,28 @@ forest = TreeNode(100,
                           TreeNode(1500, TreeNode(700), TreeNode(2600)))
 
 print(count_old_growth(forest, 1000))
+
+# Problem 8
+print("problem 8")
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+def is_identical(root1, root2):
+    if not root1 and not root2:
+        return True
+    if not root1 or not root2:
+        return False
+    if root1.val != root2.val:
+        return False
+    return is_identical(root1.left, root2.left) and is_identical(root1.right, root2.right)
+
+root1 = TreeNode(1, TreeNode(2), TreeNode(3))
+root2 = TreeNode(1, TreeNode(2), TreeNode(3))
+root3 = TreeNode(1, TreeNode(2))
+root4 = TreeNode(1, None, TreeNode(2))
+
+print(is_identical(root1, root2))
+print(is_identical(root3, root4))
