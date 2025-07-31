d552ca9a762a46ea07c7d3a78b3950583335ab8a
Enochteo <enochowoade@gmail.com>
2025-07-31 15:50:59 -0500

    add new problem

--- Wk9/session1.py
+++ Wk9/session1.py
@@ -1,63 +1,158 @@
 from collections import deque
+def print_tree(root):
+    if not root:
+        return "Empty"
+    result = []
+    queue = deque([root])
+    while queue:
+        node = queue.popleft()
+        if node:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+    while result and result[-1] is None:
+        result.pop()
+    print(result)
+# # Problem 1
+# class Puff():
+#      def __init__(self, flavor, left=None, right=None):
+#         self.val = flavor
+#         self.left = left
+#         self.right = right
 
-# Problem 1
-class Puff():
-     def __init__(self, flavor, left=None, right=None):
-        self.val = flavor
+# def listify_design(design):
+#     # Level order traversal 
+
+#     # Create a queue initially a list of the root
+#     queue = deque()
+#     queue.append(design)
+
+#     # result list
+#     result = []
+
+#     # while loop - queue
+#     while queue:
+#         # create a level list
+#         cur_level = []
+#         # get length of queue
+#         length = len(queue)
+        
+#         # do for lop
+#         for i in range(length):
+#         #   pop the queue from left
+#             node = queue.popleft()
+
+#         #   append its children
+#             if node.left:
+#                 queue.append(node.left)
+
+#             if node.right:
+#                 queue.append(node.right)
+
+#         #   append the popped to the level
+#             cur_level.append(node.val)
+
+#         # append the level to the result
+#         result.append(cur_level)
+        
+#     return result
+
+# """
+#             Vanilla
+#            /       \
+#       Chocolate   Strawberry
+#       /     \
+#   Vanilla   Matcha  
+# """
+# croquembouche = Puff("Vanilla", 
+#                     Puff("Chocolate", Puff("Vanilla"), Puff("Matcha")), 
+#                     Puff("Strawberry"))
+# print(listify_design(croquembouche))
+
+# # Time Complexity: O(n)
+# # Space: O(n)
+# def build_tree(values):
+#   if not values:
+#       return None
+
+#   def get_key_value(item):
+#       if isinstance(item, tuple):
+#           return item[0], item[1]
+#       else:
+#           return None, item
+
+#   key, value = get_key_value(values[0])
+#   root = TreeNode(value, key)
+#   queue = deque([root])
+#   index = 1
+
+#   while queue:
+#       node = queue.popleft()
+#       if index < len(values) and values[index] is not None:
+#           left_key, left_value = get_key_value(values[index])
+#           node.left = TreeNode(left_value, left_key)
+#           queue.append(node.left)
+#       index += 1
+#       if index < len(values) and values[index] is not None:
+#           right_key, right_value = get_key_value(values[index])
+#           node.right = TreeNode(right_value, right_key)
+#           queue.append(node.right)
+#       index += 1
+
+#   return root
+
+# # Problem 2
+# def zigzag_icing_order(cupcakes):
+#       # Create a queue initially a list of the root
+#     queue = deque()
+#     queue.append(cupcakes)
+#     reversed = False
+#     # result list
+#     result = []
+
+#     while queue:
+#         cur_lvl = deque()
+#         for i in range(len(queue)):
+#             node = queue.popleft()
+#             cur_lvl.append(node.val)
+#             if node.left:
+#                 queue.append(node.left)
+
+#             if node.right:
+#                 queue.append(node.right)
+
+#         if reversed:
+#             while cur_lvl:
+#                 result.append(cur_lvl.pop())
+#         else:
+#             while cur_lvl:
+#                 result.append(cur_lvl.popleft())
+
+#         reversed = not reversed
+                
+#     return result
+
+# """
+#             Chocolate
+#            /         \
+#         Vanilla       Lemon
+#        /              /    \
+#     Strawberry   Hazelnut   Red Velvet   
+# """
+
+# # Using build_tree() function included at top of page
+# flavors = ["Chocolate", "Vanilla", "Lemon", "Strawberry", None, "Hazelnut", "Red Velvet"]
+# cupcakes = build_tree(flavors)
+# print(zigzag_icing_order(cupcakes))
+
+# Problem 3
+class TreeNode():
+     def __init__(self, order_size, left=None, right=None):
+        self.val = order_size
         self.left = left
         self.right = right
-
-def listify_design(design):
-    # Level order traversal 
-
-    # Create a queue initially a list of the root
-    queue = deque()
-    queue.append(design)
-
-    # result list
-    result = []
-
-    # while loop - queue
-    while queue:
-        # create a level list
-        cur_level = []
-        # get length of queue
-        length = len(queue)
-        
-        # do for lop
-        for i in range(length):
-        #   pop the queue from left
-            node = queue.popleft()
-
-        #   append its children
-            if node.left:
-                queue.append(node.left)
-
-            if node.right:
-                queue.append(node.right)
-
-        #   append the popped to the level
-            cur_level.append(node.val)
-
-        # append the level to the result
-        result.append(cur_level)
-        
-    return result
-
-"""
-            Vanilla
-           /       \
-      Chocolate   Strawberry
-      /     \
-  Vanilla   Matcha  
-"""
-croquembouche = Puff("Vanilla", 
-                    Puff("Chocolate", Puff("Vanilla"), Puff("Matcha")), 
-                    Puff("Strawberry"))
-print(listify_design(croquembouche))
-
-# Time Complexity: O(n)
-# Space: O(n)
 def build_tree(values):
   if not values:
       return None
@@ -87,47 +182,18 @@ def build_tree(values):
       index += 1
 
   return root
+def larger_order_tree(orders):
+    def inorder(node, sum_cum):
+        if not node:
+            return sum_cum
+        sum_cum = inorder(node.right, sum_cum)
+        node.val += sum_cum
+        sum_cum = node.val
+        return inorder(node.left, sum_cum)
+    inorder(orders, 0)
+    return orders
+order_sizes = [4,1,6,0,2,5,7,None,None,None,3,None,None,None,8]
+orders = build_tree(order_sizes)
 
-# Problem 2
-def zigzag_icing_order(cupcakes):
-      # Create a queue initially a list of the root
-    queue = deque()
-    queue.append(cupcakes)
-    reversed = False
-    # result list
-    result = []
-
-    while queue:
-        cur_lvl = deque()
-        for i in range(len(queue)):
-            node = queue.popleft()
-            cur_lvl.append(node.val)
-            if node.left:
-                queue.append(node.left)
-
-            if node.right:
-                queue.append(node.right)
-
-        if reversed:
-            while cur_lvl:
-                result.append(cur_lvl.pop())
-        else:
-            while cur_lvl:
-                result.append(cur_lvl.popleft())
-
-        reversed = not reversed
-                
-    return result
-
-"""
-            Chocolate
-           /         \
-        Vanilla       Lemon
-       /              /    \
-    Strawberry   Hazelnut   Red Velvet   
-"""
-
-# Using build_tree() function included at top of page
-flavors = ["Chocolate", "Vanilla", "Lemon", "Strawberry", None, "Hazelnut", "Red Velvet"]
-cupcakes = build_tree(flavors)
-print(zigzag_icing_order(cupcakes))
+# using print_tree() function included at top of page
+print_tree(larger_order_tree(orders))
