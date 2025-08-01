015f6e37fd7e9a67d6e39c70b21ed5878f08120c
Enochteo <enochowoade@gmail.com>
2025-07-31 20:53:46 -0500

    add new problem

--- Wk9/session2.py
+++ Wk9/session2.py
@@ -92,3 +92,42 @@ descriptions2 = [
 # Using print_tree() function included at top of page
 print_tree(build_cookie_tree(descriptions1))
 print_tree(build_cookie_tree(descriptions2))
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def count_cookie_paths(root, target_sum):
+    if not root:
+        return 0
+    if not root.left and not root.right:
+        if root.val == target_sum:
+            return 1
+        else:
+            return 0
+    return count_cookie_paths(root.left, target_sum - root.val) + count_cookie_paths(root.right, target_sum - root.val)
+"""
+    10
+   /  \
+  5     8
+ / \   / \
+3   7 12  4
+"""
+# Using build_tree() function included at the top of the page
+cookie_nums = [10, 5, 8, 3, 7, 12, 4]
+cookies1 = build_tree(cookie_nums)
+
+"""
+    8
+   / \
+  4   12
+ / \    \
+2   6    10
+"""
+cookie_nums = [8, 4, 12, 2, 6, None, 10]
+cookies2 = build_tree(cookie_nums)
+
+print(count_cookie_paths(cookies1, 22)) 
+print(count_cookie_paths(cookies2, 14)) 
