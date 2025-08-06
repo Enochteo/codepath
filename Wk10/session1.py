e1f5742c638e8764598db72cc6f70f4f2ffb780c
Enochteo <enochowoade@gmail.com>
2025-08-06 01:53:51 -0500

    add new problem

--- Wk10/session1.py
+++ Wk10/session1.py
@@ -39,6 +39,7 @@ print(find_center(terminals1))
 print(find_center(terminals2))
 
 # Problem 3
+# dfs
 def get_all_destinations(flights, start):
     res = []
     visited = set()
@@ -67,3 +68,66 @@ flights = {
 print(get_all_destinations(flights, "Beijing"))
 print(get_all_destinations(flights, "Helsinki"))
 print(get_all_destinations(flights, "New York"))
+
+# Problem 4
+# DFS
+from collections import deque
+def get_all_destinations(flights, start):
+    q = deque([start])
+    res = []
+    visited = set([start])
+    while q:
+        curr = q.popleft()
+        if curr != start:
+            res.append(curr)
+        for s in flights[curr]:
+            if s not in visited:
+                visited.add(curr)
+                q.append(s)
+    return res
+flights = {
+    "Tokyo": ["Sydney"],
+    "Sydney": ["Tokyo", "Beijing"],
+    "Beijing": ["Mexico City", "Helsinki"],
+    "Helsinki": ["Cairo", "New York"],
+    "Cairo": ["Helsinki", "Reykjavik"],
+    "Reykjavik": ["Cairo", "New York"],
+    "Mexico City": ["Sydney"], 
+    "New York": []  
+}
+
+print(get_all_destinations(flights, "Beijing"))
+print(get_all_destinations(flights, "Helsinki"))
+
+
+def find_itinerary(boarding_passes):
+    map = {}
+    res = []
+    arrivals = set() # store all arrivals coz start cannot be in attivals as there are no cycles
+    for dest, arr in boarding_passes:
+        map[dest] = arr
+        arrivals.add(arr)
+    for s in map:
+        if s not in arrivals:
+            start = s
+    res.append(start)
+    while start in map:
+        start = map[start]
+        res.append(start)
+        
+    return res
+
+boarding_passes_1 = [
+                    ("JFK", "ATL"),
+                    ("SFO", "JFK"),
+                    ("ATL", "ORD"),
+                    ("LAX", "SFO")]
+
+boarding_passes_2 = [
+                    ("LAX", "DXB"),
+                    ("DFW", "JFK"),
+                    ("LHR", "DFW"),
+                    ("JFK", "LAX")]
+
+print(find_itinerary(boarding_passes_1))
+print(find_itinerary(boarding_passes_2))
